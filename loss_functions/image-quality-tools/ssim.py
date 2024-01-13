7a7e0513454fea221a5f0f81f0b1f76fe63ba77e
Sergei Volchkov <c4s23@yandex.ru>
2024-01-14 02:21:51 +0300

    С комментариями разные

--- loss_functions/image-quality-tools/ssim.py
+++ loss_functions/image-quality-tools/ssim.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy.ndimage import convolve, gaussian_filter
+from scipy.signal import convolve2d
+from scipy.ndimage import gaussian_filter
 
 
 def ssim(
@@ -9,94 +10,30 @@ def ssim(
         window=None,
         L: float = None
 ) -> tuple:
-    """% ========================================================================
-% Edited code by Adam Turcotte and Nicolas Robidoux
-% Laurentian University
-% Sudbury, ON, Canada
-% Last Modified: 2011-01-22
-% ----------------------------------------------------------------------
-% This code implements a refactored computation of SSIM that requires
-% one fewer blur (4 instead of 5), the same number of pixel-by-pixel
-% binary operations (10), and two fewer unary operations (6 instead of 8).
-%
-% In addition, this version reduces memory usage with in-place functions.
-% As a result, it supports larger input images.
-%========================================================================
+    def _fspecial_gauss(size, sigma):
+        x, y = np.mgrid[-size // 2 + 1:size // 2 + 1, -size // 2 + 1:size // 2 + 1]
+        g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
+        return g / g.sum()
 
-% ========================================================================
-% SSIM Index with automatic downsampling, Version 1.0
-% Copyright(c) 2009 Zhou Wang
-% All Rights Reserved.
-%
-% ----------------------------------------------------------------------
-% Permission to use, copy, or modify this software and its documentation
-% for educational and research purposes only and without fee is hereby
-% granted, provided that this copyright notice and the original authors'
-% names appear on all copies and supporting documentation. This program
-% shall not be used, rewritten, or adapted as the basis of a commercial
-% software or hardware product without first obtaining permission of the
-% authors. The authors make no representations about the suitability of
-% this software for any purpose. It is provided "as is" without express
-% or implied warranty.
-%----------------------------------------------------------------------
-%
-% This is an implementation of the algorithm for calculating the
-% Structural SIMilarity (SSIM) index between two images
-%
-% Please refer to the following paper and the website with suggested usage
-%
-% Z. Wang, A. C. Bovik, H. R. Sheikh, and E. P. Simoncelli, "Image
-% quality assessment: From error visibility to structural similarity,"
-% IEEE Transactios on Image Processing, vol. 13, no. 4, pp. 600-612,
-% Apr. 2004.
-%
-% http://www.ece.uwaterloo.ca/~z70wang/research/ssim/
-%
-% Note: This program is different from ssim_index.m, where no automatic
-% downsampling is performed. (downsampling was done in the above paper
-% and was described as suggested usage in the above website.)
-%
-% Kindly report any suggestions or corrections to zhouwang@ieee.org
-%
-%----------------------------------------------------------------------
-%
-%Input : (1) img1: the first image being compared
-%        (2) img2: the second image being compared
-%        (3) K: constants in the SSIM index formula (see the above
-%            reference). defualt value: K = [0.01 0.03]
-%        (4) window: local window for statistics (see the above
-%            reference). default widnow is Gaussian given by
-%            window = fspecial('gaussian', 11, 1.5);
-%        (5) L: dynamic range of the images. default: L = 255
-%
-%Output: (1) mssim: the mean SSIM index value between 2 images.
-%            If one of the images being compared is regarded as
-%            perfect quality, then mssim can be considered as the
-%            quality measure of the other image.
-%            If img1 = img2, then mssim = 1.
-%        (2) ssim_map: the SSIM index map of the test image. The map
-%            has a smaller size than the input images. The actual size
-%            depends on the window size and the downsampling factor.
-%
-%Basic Usage:
-%   Given 2 test images img1 and img2, whose dynamic range is 0-255
-%
-%   [mssim, ssim_map] = ssim(img1, img2);
-%
-%Advanced Usage:
-%   User defined parameters. For example
-%
-%   K = [0.05 0.05];
-%   window = ones(8);
-%   L = 100;
-%   [mssim, ssim_map] = ssim(img1, img2, K, window, L);
-%
-%Visualize the results:
-%
-%   mssim                        %Gives the mssim value
-%   imshow(max(0, ssim_map).^4)  %Shows the SSIM index map
-%========================================================================
-"""
+    def _matlab_style_gauss2D(shape=(3, 3), sigma=0.5):
+        """
+        2D gaussian mask - should give the same result as MATLAB's
+        fspecial('gaussian',[shape],[sigma])
+        """
+        m, n = [(ss - 1.) / 2. for ss in shape]
+        y, x = np.ogrid[-m:m + 1, -n:n + 1]
+        h = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
+        h[h < np.finfo(h.dtype).eps * h.max()] = 0
+        sumh = h.sum()
+        if sumh != 0:
+            h /= sumh
+        return h
+
+    def conv2(x, y, mode='same'):
+        return np.rot90(convolve2d(np.rot90(x, 2), np.rot90(y, 2), mode=mode), 2)
+
+    def filter2(x, y, mode='same'):
+        return conv2(x, np.rot90(y, 2), mode=mode)
 
     if img1.dtype != img2.dtype:
         raise ValueError("Input images must have the same dtype.")
@@ -113,7 +50,9 @@ def ssim(
         if (M < 11) or (N < 11):
             return -np.inf, -np.inf
 
-        window = gaussian_filter(np.ones((11, 11)), 1.5)
+        # window = gaussian_filter(np.ones((11, 11)), 1.5)
+        # window = _fspecial_gauss(11, 1.5)
+        window = _matlab_style_gauss2D((11, 11), 1.5)
         K = (0.01, 0.03)
         L = 255
 
@@ -124,8 +63,12 @@ def ssim(
 
     if f > 1:
         lpf = np.ones((f, f)) / f ** 2
-        img1 = convolve(img1, lpf, mode='reflect')
-        img2 = convolve(img2, lpf, mode='reflect')
+
+        # Необходимо заменить на python аналог
+        # img1 = imfilter(img1,lpf,'symmetric','same');
+        # img2 = imfilter(img2,lpf,'symmetric','same');
+        img1 = convolve2d(img1, lpf, mode='same', boundary='symm')
+        img2 = convolve2d(img2, lpf, mode='same', boundary='symm')
 
         img1 = img1[::f, ::f]
         img2 = img2[::f, ::f]
@@ -134,12 +77,18 @@ def ssim(
     C2 = (K[1] * L) ** 2
 
     window = window / np.sum(np.sum(window))
-    ssim_map = convolve(img1, window, mode='reflect')
-    w1 = convolve(img2, window, mode='reflect')
+    # ssim_map = convolve(img1, window, mode='reflect')
+    # w1 = convolve(img2, window, mode='reflect')
+
+    # Необходимо заменить на python аналог
+    ssim_map = filter2(window, img1, 'valid')
+    # ssim_map = convolve2d(window, img1, mode='valid')
+    w1 = filter2(window, img2, 'valid')
     w2 = ssim_map * w1
     w2 = 2 * w2 + C1
     w1 = (w1 - ssim_map) ** 2 + w2
-    ssim_map = convolve(img1 * img2, window, mode='reflect')
+    # ssim_map = convolve(img1 * img2, window, mode='reflect')
+    ssim_map = filter2(window, img1 * img2, 'valid')
     ssim_map = (2 * ssim_map + (C1 + C2)) - w2
     ssim_map *= w2
     img1 = img1 ** 2
@@ -147,12 +96,14 @@ def ssim(
     img1 = img1 + img2
 
     if C1 > 0 and C2 > 0:
-        w2 = convolve(img1, window, mode='reflect')
+        # w2 = convolve(img1, window, mode='reflect')
+        w2 = filter2(window, img1, 'valid')
         w2 = w2 - w1 + (C1 + C2)
-        w2 *= w1
+        w2 = w2 * w1
         ssim_map /= w2
     else:
-        w3 = convolve(img1, window, mode='reflect')
+        # w3 = convolve(img1, window, mode='reflect')
+        w3 = filter2(window, img1, 'valid')
         w3 = w3 - w1 + (C1 + C2)
         w4 = np.ones_like(w1)
         index = (w1 * w3 > 0)
